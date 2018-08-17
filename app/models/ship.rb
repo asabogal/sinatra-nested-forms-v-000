b7dfb03c8cb2c33d59aa20952d2c75ae9070bc88
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 18:52:30 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+
+  SHIPS = []
+
+  attr_accessor :name, :type, :booty
+
+  def initialize(args)
+    @name = params[:name]
+    @weight = params[:type]
+    @height = params[:booty]
+    SHIPS << self
+  end
+type
+  def self.all
+    SHIPS
+  end
+
+
+  
 end
