4796949f870e5f51fc6488e25d4644ee39f26d3b
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 19:50:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ module FormsLab
       erb :"/pirates/new"
     end
 
-    post '/new' do
+    post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |details|
